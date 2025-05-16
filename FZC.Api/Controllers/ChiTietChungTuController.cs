34d0a609cbc440aeb9c6a776ea9342c900f35ad7
Ta Chi Nguyen <tcnguyen93@gmail.com>
2025-05-16 16:30:35 +0700

    up

--- FZC.Api/Controllers/ChiTietChungTuController.cs
+++ FZC.Api/Controllers/ChiTietChungTuController.cs
@@ -4,12 +4,15 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FZC.Api.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class ChiTietChungTuController : ControllerBase
     {
         private readonly IChiTietChungTuRepository _chitietChungTu;
         public ChiTietChungTuController(IChiTietChungTuRepository chitietChungTu) {
             this._chitietChungTu = chitietChungTu;
         }
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             var topics = await _chitietChungTu.GetAllAsync();
--- FZC.Domain/Entities/ChiTietChungTu.cs
+++ FZC.Domain/Entities/ChiTietChungTu.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FZC.Domain.Entities
 {
+    [Table("ChiTietChungTu")]
     public class ChiTietChungTu: BaseEntity
     {
         [Key]
@@ -15,8 +17,7 @@ namespace FZC.Domain.Entities
         public string DienGiai { get; set; } = string.Empty;
         public decimal SoTien {  get; set; }    
         public string LoaiGiaoDich {  get; set; } = string.Empty;
-        
-        public int ChungTuId;
+        public int ChungTuId { get; set; }
         public virtual ChungTu ChungTu { get; set; }
     }
 }
